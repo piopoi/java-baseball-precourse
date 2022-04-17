041a3659931d71adff476e71ebd585ab31c62eaa
piopoi <kangsw1988@gmail.com>
2022-04-17 18:57:51 +0900

    feat: Main 메서드에 야구게임 플레이 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.controller.BaseballController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballController baseballController = new BaseballController();
+
+        boolean restart = true; //재시작 여부
+        while (restart) { //재시작 여부에 따라 게임을 반복한다.
+            restart = baseballController.play(); //게임 종료 후 재시작여부를 반환한다.
+        }
     }
 }
