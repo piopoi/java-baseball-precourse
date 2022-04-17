eb0e2334942f61be7849f9739589a51c99b4a984
piopoi <kangsw1988@gmail.com>
2022-04-17 22:24:49 +0900

    refactor: 게임 재시작 로직 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,9 +7,8 @@ public class Application {
         // TODO: 프로그램 구현
         BaseballController baseballController = new BaseballController(); //게임 관리 객체 생성
 
-        boolean restart = true; //재시작 여부
-        while (restart) { //재시작 여부에 따라 게임을 반복한다.
-            restart = baseballController.play(); //게임 종료 후 재시작여부를 반환한다.
-        }
+        do {
+            baseballController.play(); //게임 플레이
+        } while (baseballController.restart()); //재시작 여부에 따라 게임을 반복한다.
     }
 }
--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -12,21 +12,19 @@ import java.util.List;
 public class BaseballController {
 
     /**
-     * 야구게임 플레이
-     *
-     * @return 게임 재시작 여부
+     * 게임 플레이
      */
-    public boolean play() {
+    public void play() {
         Computer computer = new Computer(); //컴퓨터 객체 생성
         Player player = new Player(); //플레이어 객체 생성
         Judge judge = new Judge(); //심판 객체 생성
+
         List<Integer> computerNumList = computer.getComputerNumber(); //컴퓨터 랜덤 숫자 리스트 생성
         boolean threeStrike = false; //3스트라이크 여부
         while (!threeStrike) { //3스트라이크가 나올 때까지 반복
             List<Integer> playerNumList = player.input(); //플레이어의 숫자 입력
             threeStrike = judge.judgment(computerNumList, playerNumList); //결과 판정 후 3스트라이크 여부가 반환된다.
         }
-        return restart(); //게임 재시작 여부 반환
     }
 
     /**
@@ -34,7 +32,7 @@ public class BaseballController {
      *
      * @return 재시작 여부
      */
-    private boolean restart() {
+    public boolean restart() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
